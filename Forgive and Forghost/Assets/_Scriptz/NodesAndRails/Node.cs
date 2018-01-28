f05a3038cd761f3366ca8899cc34d8a5f817906f
Chloe Lister <chtlister@gmail.com>
2018-01-27 18:28:27 -0800

    Rail transitions wow. theyre kinda bad

--- Forgive and Forghost/Assets/_Scriptz/NodesAndRails/Node.cs
+++ Forgive and Forghost/Assets/_Scriptz/NodesAndRails/Node.cs
@@ -1,8 +1,18 @@
 ﻿using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Node : MonoBehaviour
 {
 	public List<Rail> rails;
+
+    public Rail getNextRailOnArrive(Rail fromRail)
+    {
+        //If there's only one rail, go to the other rail:
+        if (rails.Count == 2)
+            return this.rails.Where(rail => rail != fromRail).ToList()[0];
+        else
+            throw new System.Exception("Haven't handled this case yet");
+    }
 }
--- Forgive and Forghost/Assets/_Scriptz/PlayerGhost.cs
+++ Forgive and Forghost/Assets/_Scriptz/PlayerGhost.cs
@@ -14,6 +14,9 @@ public class PlayerGhost : MonoBehaviour {
     [SerializeField] private float twistAcceleration_c = 270f;
     [SerializeField] private float twistMaxVelocity_c = 270f;
 
+    /// Must be at this distance or less to be arrived at the node
+    [SerializeField] private float arrivedAtNodeTolerance_c = 0.5f;
+
     /*# State #*/
     private float currentSpeed = 0f;
     private float angularVelocity = 0f;
@@ -32,9 +35,10 @@ public class PlayerGhost : MonoBehaviour {
 	
 	void Update () {
         
-        // Apply acceleration and rotation:
+        // Apply acceleration:
         this.currentSpeed = Mathf.Clamp(this.currentSpeed + Time.deltaTime * this.acceleration_c, 0f, maxSpeed_c);
 
+        // Rotate:
         var twistInput = Input.GetAxis("Horizontal");
         if (Mathf.Abs(twistInput) > 0.001f){
             this.angularVelocity = Mathf.Clamp(this.angularVelocity + twistInput * this.twistAcceleration_c * Time.deltaTime, -this.twistMaxVelocity_c, this.twistMaxVelocity_c);
@@ -44,10 +48,22 @@ public class PlayerGhost : MonoBehaviour {
         }
         this.twist = (this.twist + twistInput * twistAcceleration_c * Time.deltaTime) % 360f;
 
-        // Position and Rotate body appropriately along rail:
-        this.transform.rotation = Quaternion.AngleAxis(this.twist, this.currentRail.asAxis) * Quaternion.LookRotation(this.currentRail.asAxis);
+        // Move along rail:
         this.transform.position += this.currentRail.asAxis * this.currentSpeed * Time.deltaTime;
+
+        // Check to see if we've hit the end of the rail:
+        if ((this.transform.position - this.currentRail.endNode.transform.position).sqrMagnitude <= arrivedAtNodeTolerance_c * arrivedAtNodeTolerance_c) {
+            // If so, move to the next rail:
+            this.changeRail(this.currentRail.endNode.getNextRailOnArrive(this.currentRail));
+        }
+
+        // Update my rotation along the current rail:
+        this.transform.rotation = Quaternion.AngleAxis(this.twist, this.currentRail.asAxis) * Quaternion.LookRotation(this.currentRail.asAxis);
     }
 
-
+    private void changeRail(Rail newRail)
+    {
+        this.currentRail = newRail;
+        this.transform.position = newRail.originNode.transform.position;
+    }
 }
