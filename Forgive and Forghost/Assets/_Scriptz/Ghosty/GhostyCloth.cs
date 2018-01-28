20327f60dfb912f8b44946dde083dc2f8d83db44
Sean Wejebe <we.sean94@gmail.com>
2018-01-27 17:16:59 -0800

    say hello to ghosty

---
+++ Forgive and Forghost/Assets/_Scriptz/Ghosty/GhostyCloth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostyCloth : MonoBehaviour
+{
+	[Header("References")]
+	public Cloth ghostyCloth;
+
+	[Header("Settings")]
+	public float gravity;
+
+	void Update()
+	{
+		Vector3 down = transform.TransformDirection (-transform.up);
+		down = down.normalized * gravity;
+		ghostyCloth.externalAcceleration = down;
+	}
+}
