52b0499af483ddae4174065539b8788c194ab31a
Sean Wejebe <we.sean94@gmail.com>
2018-01-27 18:23:14 -0800

    rail generation tests! huhmm.. time to fix ghosty

--- Forgive and Forghost/Assets/_Scriptz/NodesAndRails/Rail.cs
+++ Forgive and Forghost/Assets/_Scriptz/NodesAndRails/Rail.cs
@@ -6,4 +6,23 @@ public class Rail : MonoBehaviour
 {
 	public Node originNode;
 	public Node endNode;
+
+	[Header("References")]
+	public LineRenderer lineRenderer;
+
+	public void SetNodes(Node origin, Node end)
+	{
+		originNode = origin;
+		endNode = end;
+		lineRenderer.positionCount = 2;
+		lineRenderer.SetPosition(0, origin.transform.position);
+		lineRenderer.SetPosition(1, end.transform.position);
+		origin.TrackRail(this);
+		end.TrackRail(this);
+	}
+
+	public bool HasNodes()
+	{
+		return (originNode != null && endNode != null);
+	}
 }
