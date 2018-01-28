6b38f66ce4fe85c417b4278977ff34c1e1069c32
Chloe Lister <chtlister@gmail.com>
2018-01-27 17:25:31 -0800

    ghost is making their way to heaven

--- Forgive and Forghost/Assets/_Scriptz/NodesAndRails/Rail.cs
+++ Forgive and Forghost/Assets/_Scriptz/NodesAndRails/Rail.cs
@@ -2,8 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CapsuleCollider))]
 public class Rail : MonoBehaviour
 {
 	public Node originNode;
 	public Node endNode;
+
+    public float width { get; private set; }
+
+    private void Start()
+    {
+        this.width = this.GetComponent<CapsuleCollider>().radius;
+    }
+
+    public Vector3 asAxis => (endNode.transform.position - originNode.transform.position).normalized;
 }
---
+++ Forgive and Forghost/Assets/_Scriptz/PlayerGhost.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGhost : MonoBehaviour {
+
+    /*# Scene References #*/
+    [SerializeField] private Rail startingRail;
+
+    [SerializeField] private float maxSpeed = 10f;
+    [SerializeField] private float acceleration = 100f;
+
+    private float currentSpeed = 0f;
+
+    private Rail currentRail;
+    private Node nextNode;
+
+    /// Current angle around rail axis, in degrees
+    [SerializeField] private float twist;
+
+    void Start () {
+        this.currentRail = startingRail;
+        this.transform.position = this.startingRail.originNode.transform.position;
+
+	}
+	
+	void Update () {
+        // Rotate body to appropriate twist around rail:
+        this.transform.rotation = Quaternion.AngleAxis(this.twist, this.currentRail.asAxis) * Quaternion.LookRotation(this.currentRail.asAxis);
+
+        // Accelerate in rail direction:
+        this.currentSpeed = Mathf.Clamp(this.currentSpeed + Time.deltaTime * this.acceleration, 0f, maxSpeed);
+
+        // Update position along rail:
+        this.transform.position += this.currentRail.asAxis * this.currentSpeed * Time.deltaTime;
+	}
+
+
+}
