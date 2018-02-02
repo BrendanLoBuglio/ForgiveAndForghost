6dd19ca19dd2f14c7ea911e4bdbe32306846f69f
Sean Wejebe <we.sean94@gmail.com>
2018-02-01 22:31:39 -0800

    this makes it a lot easier to know where you are.. do ppl like that?? or??

---
+++ Forgive and Forghost/Assets/_Scriptz/FollowMyParentKindOf.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMyParentKindOf : MonoBehaviour
+{
+	[Header("References")]
+	public Transform positionParent;
+
+	void Awake()
+	{
+		//transform.SetParent(null);
+	}
+
+	void Update()
+	{
+		//transform.position = positionParent.position;
+	}
+}
