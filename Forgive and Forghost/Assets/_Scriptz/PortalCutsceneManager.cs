4c3f7a6869c517c3996e9728a16c7797fbbbf328
Sean Wejebe <we.sean94@gmail.com>
2018-02-05 21:57:44 -0800

    cutscene in progress!

---
+++ Forgive and Forghost/Assets/_Scriptz/PortalCutsceneManager.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCutsceneManager : MonoBehaviour
+{
+	[Header("References")]
+	[SerializeField] protected List<Transform> portalOrbs = new List<Transform>();
+	[SerializeField] protected Renderer portalRenderer;
+
+	[Header("Settings")]
+	[SerializeField] protected float originalPortalOrbLocalY;
+	[SerializeField] protected float animatePortalOrbsTo;
+	[SerializeField] protected float animatePortalOrbsTime;
+	[SerializeField] protected Ease animatePortalOrbsEase;
+
+	[SerializeField] protected float portalFadeTime;
+
+	protected bool _doingCutscene;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			StartCutscene();
+		}
+	}
+
+	protected void StartCutscene()
+	{
+		if (!_doingCutscene)
+		{
+			_doingCutscene = true;
+
+			for (int i = 0; i < portalOrbs.Count; i++)
+			{
+				Transform orb = portalOrbs[i];
+				orb.DOLocalMoveY(animatePortalOrbsTo, animatePortalOrbsTime).SetEase(animatePortalOrbsEase);
+			}
+
+			DOTween.Sequence().AppendInterval(animatePortalOrbsTime).AppendCallback(FadeInPortal);
+		}
+	}
+
+	protected void FadeInPortal()
+	{
+		portalRenderer.gameObject.SetActive(true);
+		portalRenderer.material.DOFade(0, portalFadeTime).From();
+	}
+}
